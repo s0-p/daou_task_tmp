82e7a8a66827e1263bd52b8aac4261d3dc82331b
s0-p <sysy0429@daou.co.kr>
2022-08-01 17:30:59 +0900

    sort

--- src/main/java/com/gwd/traceTool/Controller/TraceController.java
+++ src/main/java/com/gwd/traceTool/Controller/TraceController.java
@@ -17,8 +17,6 @@ public class TraceController {
     public String home(Model model){
         String path = "dags_feign.2022-07-14.log";
 
-
-
         apiParser = new ApiParser();
         apiParser.readApi(path);
 
@@ -30,6 +28,9 @@ public class TraceController {
     public String read(Model model, String data){
         // String data; (get에서 들어온 날짜 인자값)
         // [GET] http://localhost:8080/api/analyze/trace-log?date=20220722
-            return "";
+
+        //String fileDate = "dags_feign."+data.substring(0, 3) + '-' + data.substring(4, 5) + '-' + data.substring(6, 7) + ".log";
+        //apiParser.readApi(fileDate);
+        return "";
     }
 }
